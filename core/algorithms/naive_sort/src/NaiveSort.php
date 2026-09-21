afc0558015cf65fc8009195812ad43cb5beafeba
yorche3 <hyaoki123@gmail.com>
2026-09-20 20:06:06 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/NaiveSort.php
+++ core/algorithms/naive_sort/src/NaiveSort.php
@@ -19,5 +19,53 @@
 
 final class NaiveSort
 {
-    // Pendiente: selectionSort, bubbleSort e insertionSort según el contrato.
+    public static function selectionSort(array $arr): array
+    {
+        $n = count($arr);
+        for ($i = 0; $i < $n - 1; $i++) {
+            $minIndex = $i;
+            for ($j = $i + 1; $j < $n; $j++) {
+                if ($arr[$j] < $arr[$minIndex]) {
+                    $minIndex = $j;
+                }
+            }
+            if ($minIndex !== $i) {
+                [$arr[$i], $arr[$minIndex]] = [$arr[$minIndex], $arr[$i]];
+            }
+        }
+        return $arr;
+    }
+
+    public static function bubbleSort(array $arr): array
+    {
+        $n = count($arr);
+        for ($i = 0; $i < $n - 1; $i++) {
+            $swapped = false;
+            for ($j = 0; $j < $n - $i - 1; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    [$arr[$j], $arr[$j + 1]] = [$arr[$j + 1], $arr[$j]];
+                    $swapped = true;
+                }
+            }
+            if (!$swapped) {
+                break;
+            }
+        }
+        return $arr;
+    }
+
+    public static function insertionSort(array $arr): array
+    {
+        $n = count($arr);
+        for ($i = 1; $i < $n; $i++) {
+            $key = $arr[$i];
+            $j = $i - 1;
+            while ($j >= 0 && $arr[$j] > $key) {
+                $arr[$j + 1] = $arr[$j];
+                $j--;
+            }
+            $arr[$j + 1] = $key;
+        }
+        return $arr;
+    }
 }
